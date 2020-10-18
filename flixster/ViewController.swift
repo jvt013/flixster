2f2c5fd523d360a9e3b04f4cb8ea9535b58e47e7
Jenny Trinh <jvt013@ucsd.edu>
2020-10-18 13:11:12 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Jennifer Trinh on 10/18/20.
+//  Copyright © 2020 jvt013. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
