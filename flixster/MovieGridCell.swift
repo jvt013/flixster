395f72d7dcd3808d34f888b80537f90f15c414b5
Jenny Trinh <jvt013@ucsd.edu>
2020-10-25 21:48:40 -0700

    Superhero Collection View

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jennifer Trinh on 10/25/20.
+//  Copyright © 2020 jvt013. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
